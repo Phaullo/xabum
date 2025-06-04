665232e85f8fbea2d7b0b187107cbfdf0d645338
Phaullo <phaulloaugusto@gmail.com>
2025-06-04 20:27:00 -0300

    Ajuste para usar a API

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,33 +1,41 @@
-const todosProdutos = [
-    { id: 1, nome: "Notebook Gamer i5", valor: 3999.00 },
-    { id: 2, nome: "Mouse Gamer RGB", valor: 189.90 },
-    { id: 3, nome: "Teclado Mecânico ABNT2", valor: 299.00 },
-    { id: 4, nome: "Monitor 24'' Full HD", valor: 799.99 },
-    { id: 5, nome: "Headset com microfone", valor: 249.50 },
-    { id: 6, nome: "Webcam Full HD", valor: 199.00 },
-    { id: 7, nome: "Cadeira Gamer Reclinável", valor: 1299.00 },
-    { id: 8, nome: "Placa de Vídeo RTX 3060", valor: 2799.00 },
-    { id: 9, nome: "HD SSD 1TB", valor: 499.00 },
-    { id: 10, nome: "Fonte 650W 80 Plus Bronze", valor: 389.90 }
-];
+const todosProdutos = [];
+
+async function showSection(id) {
+  console.log('Página clicada: ', id)
+  document.querySelectorAll('section').forEach(sec => {
+    sec.classList.remove('active');
+  });
+  document.getElementById(id).classList.add('active');
+  
+  if (id=='produtos'){
+    const consultarBackEnd = await fetch('http://localhost:3000/v1/produtos' )
+    const converteJSON = await consultarBackEnd.json()
+    console.log(converteJSON)
+    converteJSON.forEach( item => todosProdutos.push(item))
+
+    exibirProdutos(todosProdutos)
+    
+  }
+}
+
 function exibirProdutos(listaProdutos) {
-    const divCentral = document.getElementById('todosProdutos')
-    divCentral.innerHTML =""
+  const divCentral = document.getElementById('todosProdutos')
+  divCentral.innerHTML = ""
 
-    listaProdutos.forEach((produto) => {
-        let criarCard = document.createElement('div')
-        criarCard.classList.add('product-card');
+  listaProdutos.forEach((produto) => {
+    let criarCard = document.createElement('div')
+    criarCard.classList.add('product-card');
 
-        let criarH3 = document.createElement('h3')
-        criarH3.textContent = produto.nome
+    let criarH3 = document.createElement('h3')
+    criarH3.textContent = produto.nome
 
-        let criarP = document.createElement('p')
-        criarP.textContent = produto.valor
+    let criarP = document.createElement('p')
+    criarP.textContent = produto.valor
 
-        criarH3.appendChild(criarP)
-        criarCard.appendChild(criarH3)
-        divCentral.appendChild(criarCard)
-    })
+    criarH3.appendChild(criarP)
+    criarCard.appendChild(criarH3)
+    divCentral.appendChild(criarCard)
+  })
 }
 
 
@@ -45,5 +53,4 @@ function configurarBusca() {
   });
 }
 
-exibirProdutos(todosProdutos)
 configurarBusca() 
